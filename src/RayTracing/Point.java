246d1f622959f6122c2beb81c1c4147a5e0c0570
Gai Greenberg <gaigreenberg@mail.tau.ac.il>
2021-04-01 18:57:38 +0300

    Vector class updated new Point class

---
+++ src/RayTracing/Point.java
@@ -0,0 +1,13 @@
+package RayTracing;
+
+public class Point {
+    public float X;
+    public float Y;
+    public float Z;
+
+    public Point(float x, float y, float z){
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+}
--- src/RayTracing/Scene.java
+++ src/RayTracing/Scene.java
@@ -5,10 +5,15 @@ import java.util.List;
 public class Scene {
     public Camera Camera;
     public GeneralSettings Settings;
-    public List<Material>  Materials;
+    public List<Material> Materials;
     public List<Sphere> Shperes;
-    public List<Plane> Planes ;
+    public List<Plane> Planes;
     public List<Box> Boxes;
-    public List<Light> Lights ;
+    public List<Light> Lights;
+    public Vector Vx;
+    public Vector Vy;
 
+    // to be continued
+    //public static Point FindP0();
 }
+
