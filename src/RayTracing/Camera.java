e4af4bd0a91f635d25a5201e48623dfff7a8e15a
Gai Greenberg <gaigreenberg@mail.tau.ac.il>
2021-04-08 10:31:18 +0300

    position on screen updated

--- src/RayTracing/Camera.java
+++ src/RayTracing/Camera.java
@@ -3,21 +3,24 @@ package RayTracing;
 public class Camera {
     public Vector Position;
     public Vector LookAtPoint;
+    public Vector TowardsVector;
     public Vector UpVector;
     public double ScreenDistance;
     public double ScreenWidth;
     public double K;
     public Vector RightVector;
+    public Vector Dx;
+    public Vector Dy;
 
     /*
     init Direction Vectors according to what we saw in the lecture
      */
     public void InitDirectionVectors(){
-        //Vector temp = Vector.VectorSubtraction(this.LookAtPoint, this.Position);
-        Vector temp = Vector.VectorSubtraction(this.Position, this.LookAtPoint);
-        this.LookAtPoint = Vector.NormalVector(temp);
-        this.UpVector.Normalize();
-         temp = Vector.CrossProduct(this.UpVector, this.LookAtPoint);
+        Vector temp = Vector.VectorSubtraction(this.LookAtPoint, this.Position);
+        this.TowardsVector = Vector.NormalVector(temp);
+        this.TowardsVector.Normalize();
+         temp = Vector.CrossProduct(this.UpVector, this.TowardsVector);
         this.RightVector = Vector.NormalVector(temp);
+
     }
 }
